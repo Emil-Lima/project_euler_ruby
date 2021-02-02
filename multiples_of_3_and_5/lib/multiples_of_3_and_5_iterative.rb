2a955f720da942efccf35a975e6ee9f98a7a01b0
J-Rozas <jrozaslima@gmail.com>
2021-02-02 23:53:39 +0000

    Refactoring and styling.

--- multiples_of_3_and_5/lib/multiples_of_3_and_5_iterative.rb
+++ multiples_of_3_and_5/lib/multiples_of_3_and_5_iterative.rb
@@ -1,10 +1,10 @@
+# frozen_string_literal: true
+
 def multiples(num)
   holder = 0
   sum = 0
   while holder < num
-    if holder % 5 == 0 || holder % 3 == 0
-      sum += holder
-    end
+    sum += holder if (holder % 5).zero? || (holder % 3).zero?
     holder += 1
   end
   sum
--- multiples_of_3_and_5/spec/mult_3and5_iterative_spec.rb
+++ multiples_of_3_and_5/spec/mult_3and5_iterative_spec.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 require './lib/multiples_of_3_and_5_iterative'
 
-describe "#multiples(num)" do
-  it "return sum of numbers below num that are multiple of 3 and/or 5." do
+describe '#multiples(num)' do
+  it 'return sum of numbers below num that are multiple of 3 and/or 5.' do
     expect(multiples(10)).to eql(23)
   end
-  it "return sum of numbers below num that are multiple of 3 and/or 5." do
+  it 'return sum of numbers below num that are multiple of 3 and/or 5.' do
     expect(multiples(0)).to eql(0)
   end
-  it "return sum of numbers below num that are multiple of 3 and/or 5." do
+  it 'return sum of numbers below num that are multiple of 3 and/or 5.' do
     expect(multiples(20)).to eql(78)
   end
 end
