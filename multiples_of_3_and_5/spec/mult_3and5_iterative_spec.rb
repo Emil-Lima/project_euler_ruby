edd1074a3251907fab30c525f05f00fa2fa89379
J-Rozas <jrozaslima@gmail.com>
2021-02-02 23:41:00 +0000

    Added testing for the iterative version.

---
+++ multiples_of_3_and_5/spec/mult_3and5_iterative_spec.rb
@@ -0,0 +1,13 @@
+require './lib/multiples_of_3_and_5_iterative'
+
+describe "#multiples(num)" do
+  it "return sum of numbers below num that are multiple of 3 and/or 5." do
+    expect(multiples(10)).to eql(23)
+  end
+  it "return sum of numbers below num that are multiple of 3 and/or 5." do
+    expect(multiples(0)).to eql(0)
+  end
+  it "return sum of numbers below num that are multiple of 3 and/or 5." do
+    expect(multiples(20)).to eql(78)
+  end
+end
