8730809adb92e7749bc65d0b356d4279d8a9a035
J-Rozas <jrozaslima@gmail.com>
2021-02-02 23:53:19 +0000

    Improved the code to get a more accurate output.

--- multiples_of_3_and_5/lib/multiples_of_3_and_5_recursive.rb
+++ multiples_of_3_and_5/lib/multiples_of_3_and_5_recursive.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
-def multiples(num-1)
-  if num == 3
+def multiples(num)
+  if num <= 0
+    0
+  elsif num == 3
     num
   elsif (num % 3).zero? || (num % 5).zero?
     num + multiples(num - 1)
   else
     multiples(num - 1)
   end
+end
+
+# We want to get the multiples of 3 and 5 that are smaller than the given number, so that we use this helper method.
+def smaller_than_num(num)
+  num -= 1
+  multiples(num)
 end
